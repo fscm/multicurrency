9b714c535c6505551809d90261668bfa7c515729
Frederico Martins <fredericoscm@gmail.com>
2021-04-16 18:24:26 +0100

    version bumped

--- multicurrency/__init__.py
+++ multicurrency/__init__.py
@@ -880,4 +880,4 @@ __author__: Final[str] = 'Frederico Martins'
 __author_email__: Final[str] = 'fscm@users.noreply.github.com'
 __license__: Final[str] = 'MIT'
 __project__: Final[str] = __package__
-__version__: Final[str] = '0.2.0'
+__version__: Final[str] = '0.3.0'
