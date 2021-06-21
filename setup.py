059b621953dcee4f38c306218c85815e489957c8
Frederico Martins <fredericoscm@gmail.com>
2021-06-21 15:18:18 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -12,13 +12,11 @@ from multicurrency import (
     __author__, __license__, __project__, __version__)
 
 
+DESCRIPTION = 'Currency representation library.'
 KEYWORDS = ['currency', 'library', 'monetary']
 
 with open('requirements.txt', 'r', encoding='utf-8') as r:
     DEPENDENCIES = [p.strip() for p in r if not p.strip().startswith('#')]
-
-with open('README.md', 'r', encoding='utf-8') as d:
-    LONG_DESCRIPTION = d.read()
 
 if __name__ == '__main__':
     setup(
@@ -29,16 +27,20 @@ if __name__ == '__main__':
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Topic :: Software Development :: Libraries',
             'Typing :: Typed'],
-        description=('Currency representation library.'),
+        description=DESCRIPTION,
         entry_points={},
         install_requires=DEPENDENCIES,
         keywords=KEYWORDS,
         license=__license__,
         license_files=['LICENSE'],
-        long_description=LONG_DESCRIPTION,
+        long_description=open('README.md', 'r', encoding='utf-8').read(),
         long_description_content_type='text/markdown',
         name=__project__,
         package_data={__project__: ['py.typed', '*.pyi']},
